98cc0a2c36761c3a342c630a5e4c34a22d6b89b8
Francilene Lopes <106615756+FrancileneLopes@users.noreply.github.com>
2023-06-03 13:36:27 -0300

    Criando formulário de login com validação
    Criação do formulário de login com validação ligada ao Banco de Dados Local. Utilizando o windows como sistema operacional e o WAMP

---
+++ sga/login/paineladm.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>painel</title>
+</head>
+<body>
+<h1>Deu certo hein pai! Seja bem vindo ao sistema!</h1>
+</body>
+</html>
