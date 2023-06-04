556925eeaada168e21725b57349416f7fe6bb7b0
Francilene Lopes <106615756+FrancileneLopes@users.noreply.github.com>
2023-06-04 19:07:17 -0300

    Adicionando e referenciando links para novas pags

--- sga/login/paineladm.php
+++ sga/login/paineladm.php
@@ -6,6 +6,10 @@
 	<title>painel</title>
 </head>
 <body>
-<h1>Deu certo hein pai! Seja bem vindo ao sistema!</h1>
+<h1>Seja bem vindo ao SGA! - Administrador</h1>
+<hr>
+
+<h3>* <a href="/sga/adm/funcionario.php">Funcionários</a></h3>
+<h3>* <a href="/sga/adm/cargos.php">Cargos</a></h3>
 </body>
 </html>
